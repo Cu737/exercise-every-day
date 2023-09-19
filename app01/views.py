3f59c57b00ce02667dcd3d327a34381888adbcea
yitong bian <cucucu737@163.com~>
2023-09-19 21:51:02 +0800

    搭建网页初始框架，完成母版书写，完成一部分选择功能界面的html和css样式书写。

---
+++ app01/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+def login(request):
+    """
+    转入登录页面
+    :param request:
+    :return:
+    """
+    return render(request, "login.html")
+
+
+def index(request):
+    """
+    转入主页
+    :param request:
+    :return:
+    """
+    return render(request, "index.html")
