698a78e187a4216eaab9fc9690dc42c851cf8c5b
yitong bian <cucucu737@163.com~>
2023-12-03 13:53:03 +0800

    设定得分计算随着计时器开始。

--- ExerciseEveryDay/local_settings.py
+++ ExerciseEveryDay/local_settings.py
@@ -3,9 +3,9 @@ LANGUAGE_CODE = "zh-hans"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'db1',
-        'USER': 'root',
-        'PASSWORD': 'asdqaz123',
+        'NAME': 'exercise_everyday',
+        'USER': 'myuser',
+        'PASSWORD': '1234567',
         'HOST': '127.0.0.1',
         'PORT': 3306
     }
